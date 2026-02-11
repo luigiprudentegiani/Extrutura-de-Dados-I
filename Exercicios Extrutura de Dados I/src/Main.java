b1fd0530e8794317e36c7872a0913effa29c81b5
luigiprudentegiani <luigiprudentegiani@gmail.com>
2026-02-11 19:16:28 -0300

    Add files via upload

---
+++ Exercicios Extrutura de Dados I/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        int [][] matriz = new int[5][5];
+        matriz [0][0] = 1;
+        matriz [1][1] = 2;
+        matriz [2][2] = 3;
+        matriz [3][3] = 4;
+        matriz [4][4] = 5;
+        for (int i = 0; i < 5; i++){
+            System.out.println(matriz [i][i]);}
+    }}
